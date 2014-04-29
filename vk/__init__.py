c8e35324e22d24284816c0f2be9e500672aa138d
dimka665 <dimka665@gmail.com>
2014-04-29 16:25:40 +0400

    Added EnterCaptchaAPI class

--- vk/__init__.py
+++ vk/__init__.py
@@ -1,5 +1,8 @@
 from vk.api import APISession as API
 from vk.api import VkError, VkAuthorizationError, VkAPIMethodError
 
+from vk.mixins import EnterCaptchaMixin
 
 
+class EnterCaptchaAPI(EnterCaptchaMixin, API):
+    pass
---
+++ vk/mixins.py
@@ -0,0 +1,20 @@
+
+
+try:
+    input = raw_input  # Python 2
+except NameError:
+    pass
+
+
+class EnterCaptchaMixin(object):
+
+    def captcha_is_needed(self, error_data, method_name, **method_kwargs):
+        captcha_sid = error_data.get('captcha_sid')
+        captcha_img = error_data.get('captcha_img')
+
+        print('Captcha URL: {}'.format(captcha_img))
+        captcha_key = input('Enter captcha text:')
+
+        method_kwargs['captcha_sid'] = captcha_sid
+        method_kwargs['captcha_key'] = captcha_key
+        return self(method_name, **method_kwargs)
