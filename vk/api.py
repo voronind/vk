5483d17ae10aa1cb8d068964937f4b4aa73ba4db
Dmitry Voronin <dimka665@gmail.com>
2015-03-25 12:51:38 +0300

    Refactored VkAPIMethodError.redirect_uri

--- vk/api.py
+++ vk/api.py
@@ -28,7 +28,7 @@ def json_iter_parse(response_text):
     idx = 0
     while idx < len(response_text):
         obj, idx = decoder.raw_decode(response_text, idx)
-        yield obj   
+        yield obj
 
 
 class APISession(object):
@@ -220,15 +220,18 @@ class VkAuthorizationError(VkError):
 
 
 class VkAPIMethodError(VkError):
-    __slots__ = ['error', 'code']
+    __slots__ = ['error', 'code', 'message', 'request_params', 'redirect_uri']
 
     def __init__(self, error):
+        super(VkAPIMethodError, self).__init__()
         self.error = error
-        self.code = error['error_code']
-        super(Exception, self).__init__()
+        self.code = error.get('error_code')
+        self.message = error.get('error_msg')
+        self.request_params = error.get('request_params')
+        self.redirect_uri = error.get('redirect_uri')
 
     def __str__(self):
-        if 'redirect_uri' in self.error.keys():
-            return "{error_code}. {error_msg}. redirect uri = {redirect_uri}, params = {request_params}".format(**self.error)
-        else:
-            return "{error_code}. {error_msg}. params = {request_params}".format(**self.error)
+        error_message = '{self.code}. {self.message}. request_params = {self.request_params}'.format(self=self)
+        if self.redirect_uri:
+            error_message += ',\nredirect_uri = "{self.redirect_uri}"'.format(self=self)
+        return error_message
