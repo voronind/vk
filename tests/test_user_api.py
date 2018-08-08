6ec05480af6e13ccd030a82d27890b407dea372e
Dmitry Voronin <dimka665@gmail.com>
2018-08-09 01:38:50 +0400

    Refactor tests and CI

--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -1,27 +1,29 @@
-import pytest
+from pytest import fixture, mark
+
+import vk
 
 
-@pytest.fixture
+@fixture
 def user_login():
     return 'user-login'
 
 
-@pytest.fixture
+@fixture
 def user_password():
     return 'user-password'
 
 
-@pytest.fixture
+@fixture
 def app_id():
     return 'app-id'
 
 
-@pytest.fixture
+@fixture
 def scope():
     return 'scope'
 
 
-@pytest.fixture(autouse=True)
+@fixture(autouse=True)
 def mock_requests_session(monkeypatch, user_login, user_password, access_token, response_class, session_class):
 
     class MockedSession(session_class):
@@ -47,3 +49,13 @@ def mock_requests_session(monkeypatch, user_login, user_password, access_token, 
             raise NotImplementedError
 
     monkeypatch.setattr('requests.Session', MockedSession)
+
+
+@mark.skip
+def test_login(user_login, user_password, app_id, scope, access_token):
+
+    api = vk.API(user_login=user_login, user_password=user_password, app_id=app_id, scope=scope)
+    assert 'access_token' not in api._session.method_default_params
+
+    api._session.update_access_token()
+    assert api._session.method_default_params.get('access_token') == access_token
