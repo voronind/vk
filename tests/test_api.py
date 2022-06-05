c437aeb9097c962c6f46e49a6eab33c1adf8729f
YariKartoshe4ka <49284924+YariKartoshe4ka@users.noreply.github.com>
2022-06-05 20:34:19 +0300

    Log warning if errors are occured during the execute method (#127)

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 
 from vk import API
@@ -9,8 +11,17 @@ def api(access_token, v):
 
 
 def test_durov(api):
-    """
-    Get users
-    """
-    users = api.users.get(user_id=1)
+    users = api.users.get(user_ids=1)
     assert users[0]['last_name'] == 'Durov'
+
+
+def test_execute(caplog, api):
+    with caplog.at_level(logging.WARNING, logger='vk'):
+        api.execute(code='var x = API.storage.get(); '
+                         'return API.users.get({user_ids: 1});')
+
+    assert len(caplog.records) == 1
+
+    log = caplog.records[0]
+    assert log.levelno == logging.WARNING
+    assert 'Execute "storage.get" error' in log.message
