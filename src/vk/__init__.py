c3cfe06e453c3cc508134662963b5829aae23219
YariKartoshe4ka <yaroslav.kikel.06@inbox.ru>
2022-07-04 10:40:38 +0300

    Bump version

--- src/vk/__init__.py
+++ src/vk/__init__.py
@@ -4,6 +4,6 @@ from .session import API, CommunityAPI, DirectUserAPI, UserAPI
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__version__ = '3.0.dev2'
+__version__ = '3.0'
 
 __all__ = (API, UserAPI, DirectUserAPI, CommunityAPI)
